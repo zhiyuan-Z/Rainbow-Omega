a819827ae392bdfd21275bd2850313c35274ea5c
Zhiyuan Zhang <zhiyuan.zhang2000@outlook.com>
2022-07-27 02:33:15 -0400

    upload Omega Release of Android application Rainbow

---
+++ app/src/main/java/com/example/testproject/Feedback.java
@@ -0,0 +1,24 @@
+package com.example.testproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Feedback extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+
+    public void startDetection(View view){
+        startActivity(new Intent(this, ColorDetectionActivity.class));
+    }
+
+    public void startMain(View view){
+        startActivity(new Intent(this, MainPage.class));
+    }
+}
